f7431eeafe3313022306c1f2d0b12ab0044a5b40
Bombowy <patryk.kulpok@vp.pl>
2022-07-16 18:12:04 +0200

    End authentication sign in and sign out with email and with google

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react"
+import {  useState } from "react"
 
 import FormInput from "../form-input/form-input.component"
 import Button from "../button/button.component"
+
+
 
 import {
 	signInWithGooglePopup,
@@ -19,23 +21,23 @@ const defaultFormFields = {
 const SignInForm = () => {
 	const [formFields, setFormFields] = useState(defaultFormFields)
 	const { email, password } = formFields
-
-	// console.log(formFields)
+	
+	
 
 	const resetFormFields = () => {
 		setFormFields(defaultFormFields)
 	}
 	const signInWithGoogle = async () => {
-		const { user } = await signInWithGooglePopup()
-		await createUserDocumentFromAuth(user)
+		await signInWithGooglePopup()
+		
 	}
 
 	const handleSubmit = async (event) => {
 		event.preventDefault()
 
 		try {
-			const response = await signInAuthUserWithEmailAndPassword(email, password)
-			console.log(response)
+			const {user} = await signInAuthUserWithEmailAndPassword(email, password)
+			
 			resetFormFields()
 		} catch (error) {
 			if (error.code === "auth/user-not-found" || "auth/wrong-password") {
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, } from "react"
 
 import FormInput from "../form-input/form-input.component"
 import Button from "../button/button.component"
@@ -8,7 +8,8 @@ import {
 	createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils"
 
-import './sign-up-form.styles.scss'
+
+import "./sign-up-form.styles.scss"
 
 const defaultFormFields = {
 	displayName: "",
@@ -21,7 +22,7 @@ const SignUpForm = () => {
 	const [formFields, setFormFields] = useState(defaultFormFields)
 	const { displayName, email, password, confirmPassword } = formFields
 
-	// console.log(formFields)
+
 
 	const resetFormFields = () => {
 		setFormFields(defaultFormFields)
@@ -39,6 +40,7 @@ const SignUpForm = () => {
 			const { user } = await createAuthUserWithEmailAndPassword(email, password)
 
 			await createUserDocumentFromAuth(user, { displayName })
+
 			resetFormFields()
 		} catch (error) {
 			if (error.code === "auth/email-already-in-use") {
@@ -55,8 +57,8 @@ const SignUpForm = () => {
 		setFormFields({ ...formFields, [name]: value })
 	}
 	return (
-		<div className="sign-up-container">
-		<h2>Don't have an account?</h2>
+		<div className='sign-up-container'>
+			<h2>Don't have an account?</h2>
 			<span>Sign up with your email and password</span>
 			<form onSubmit={handleSubmit}>
 				<FormInput
